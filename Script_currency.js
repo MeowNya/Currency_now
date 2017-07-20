1167a5c67a4f1ce71749616c505e3d994e86de6b
MeowMya <Evina666@gmail.com>
2017-07-21 01:54:11 +0500

    Initial commit

---
+++ Script_currency.js
@@ -0,0 +1,5 @@
+$(document).ready ( function(){
+    var now = new Date();
+    document.getElementById('Text_date').innerHTML = now.getHours()+":"+now.getMinutes()+":"+now.getSeconds();
+    document.qu
+})
